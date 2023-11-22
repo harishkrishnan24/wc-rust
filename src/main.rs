6768def621185674eb86b2ff12c6a20ce80959e2
Harish Krishnan <harishkrishnan1993@gmail.com>
2023-11-22 19:47:53 +0530

    print the number of bytes as output

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,21 @@
-use std::env;
+use std::{env, fs};
 
 fn main() {
     let args: Vec<String> = env::args().skip(1).collect();
-    println!("{:?}", args);
+
+    if args.len() != 2 {
+        panic!("invalid arguments");
+    }
+
+    match args[0].as_str() {
+        "-c" => {
+            let filename = &args[1];
+            let contents = fs::read(filename);
+            match contents {
+                Ok(data) => println!("{} {}", data.len(), filename),
+                Err(e) => panic!("{e}"),
+            }
+        }
+        _ => panic!("invalid arguments"),
+    };
 }
